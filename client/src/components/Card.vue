908e83e58c9f3fc5cc71f709bae905c5fbe1bd75
Subcube <maxbelf1@gmail.com>
2020-03-04 23:40:27 +0300

    added front to project

---
+++ client/src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{title}}</h1>
+    <div class="text">{{text}}</div>
+    <button class="del">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'Card',
+ props:{
+     title: {type:String, default:'fdf'},
+     text: {type:String, default:'fdf'}
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-around;
+    flex-direction: column;
+    width: 30%;
+    min-height: 200px;
+    border: 1px solid black;
+    margin: 5px;
+    padding: 5px;
+  }
+</style>
---
+++ client/src/views/About.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="content">
+    <Card 
+    v-for="item in state.items" 
+    :key="item.id"
+    :text="item.text"
+    :title="item.title"
+    />
+
+  </div>
+</template>
+
+<script>
+import Card from '../components/Card';
+export default {
+  components: {Card},
+  name: 'About',
+  data(){
+    return{
+      state: {
+        items: [
+          {id:1, title:'sasas', text:'eeeeee'},
+          {id:2, title:'ssa', text:'tttttt'},
+          {id:3, title:'saszxczxcas', text:'wwwww'},
+          {id:4, title:'sasvcxas', text:'erer'},
+          {id:5, title:'xxvx', text:'sdsdsfgddsd'},
+          {id:6, title:'vbv', text:'wewe'},
+          {id:7, title:'nbn', text:'tttttt'},
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .content{
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+</style>>
+
