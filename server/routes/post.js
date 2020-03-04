2c0fd58f8d0260e7a37d7733d5c52f311e455cb3
Subcube <maxbelf1@gmail.com>
2020-03-04 21:39:10 +0300

    delete front

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const Post = require('../models/Post')
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
 
 //http://localhost:3000/api/post (GET)
 router.get('/', async (req, res)=>{
 
-    const posts = await Post.fing({})
-    res.status(200).json(posts)
+    const posts = await Post.fing({});
+    res.status(200).json(posts);
 })
 
 //http://localhost:3000/api/post (POST)
@@ -15,17 +15,17 @@ router.get('/', async (req, res)=>{
         title: req.body.title,
         text: req.body.text
     }
-    const post = new Post(postData)
-    await post.save()
-    res.status(201).json(post)
+    const post = new Post(postData);
+    await post.save();
+    res.status(201).json(post);
 })
 
 //http://localhost:3000/api/post (DELETE)
 router.get('/:id',  async (req, res)=>{
-   await Post.remove({_id: req.params.id})
+   await Post.remove({_id: req.params.id});
     res.status(200).json({
         message: 'deleted'
-    })
-})
+    });
+});
 
-module.exports = router
+module.exports = router;
